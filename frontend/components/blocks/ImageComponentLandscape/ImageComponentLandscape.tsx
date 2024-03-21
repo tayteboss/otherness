a21823a2447962964ece04bb1f62f43a215459fb
Tayte Boss <boss.tayte@gmail.com>
2024-03-21 15:06:07 +0000

    [style] Landscape - Structure and responsive styling

--- frontend/components/blocks/ImageComponentLandscape/ImageComponentLandscape.tsx
+++ frontend/components/blocks/ImageComponentLandscape/ImageComponentLandscape.tsx
@@ -1,11 +1,54 @@
 import styled from 'styled-components';
+import LayoutWrapper from '../../common/LayoutWrapper';
+import LayoutGrid from '../../common/LayoutGrid';
+import MediaStack from '../../common/MediaStack';
+
+type Props = {
+	imageComponentLandscape: any;
+};
 
 const ImageComponentLandscapeWrapper = styled.section``;
 
-const ImageComponentLandscape = () => {
+const LandscapeWrapper = styled.div<{ $gridColumn: string }>`
+	grid-column: ${(props) => props.$gridColumn};
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: 1 / -1;
+	}
+
+	.image-component-wrapper,
+	.video-component-wrapper {
+		padding-top: 62.3%;
+	}
+`;
+
+const ImageComponentLandscape = (props: Props) => {
+	const { imageComponentLandscape } = props;
+	const { image, selectPosition, video, mediaType } = imageComponentLandscape;
+
+	const mediaData = {
+		image,
+		video,
+		mediaType
+	};
+
+	let gridColumn = '';
+
+	if (selectPosition === 'left') {
+		gridColumn = '1 / 17';
+	} else if (selectPosition === 'right') {
+		gridColumn = '-17 / -1';
+	}
+
 	return (
 		<ImageComponentLandscapeWrapper>
-			ImageComponentLandscape
+			<LayoutWrapper useGalleryLayout>
+				<LayoutGrid useGalleryGrid>
+					<LandscapeWrapper $gridColumn={gridColumn}>
+						<MediaStack data={mediaData} />
+					</LandscapeWrapper>
+				</LayoutGrid>
+			</LayoutWrapper>
 		</ImageComponentLandscapeWrapper>
 	);
 };
