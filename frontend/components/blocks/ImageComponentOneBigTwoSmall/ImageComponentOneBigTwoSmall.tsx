48bebda18ce0cd7a566056db7cb64641dcf389cc
Tayte Boss <boss.tayte@gmail.com>
2024-03-21 14:09:44 +0000

    [style] One Big Two Small - Structure and responsive styling

--- frontend/components/blocks/ImageComponentOneBigTwoSmall/ImageComponentOneBigTwoSmall.tsx
+++ frontend/components/blocks/ImageComponentOneBigTwoSmall/ImageComponentOneBigTwoSmall.tsx
@@ -1,11 +1,66 @@
 import styled from 'styled-components';
+import LayoutWrapper from '../../common/LayoutWrapper';
+import LayoutGrid from '../../common/LayoutGrid';
+import MediaStack from '../../common/MediaStack';
 
-const ImageComponentOneBigTwoSmallWrapper = styled.section``;
+type Props = {
+	imageComponentOneBigTwoSmall: {
+		big: any;
+		small1: any;
+		small2: any;
+	};
+};
 
-const ImageComponentOneBigTwoSmall = () => {
+const ImageComponentOneBigTwoSmallWrapper = styled.section`
+	.image-component-wrapper,
+	.video-component-wrapper {
+		padding-top: 100%;
+	}
+`;
+
+const BigWrapper = styled.div`
+	grid-column: span 12;
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: 1 / -1;
+	}
+`;
+
+const SmallOneWrapper = styled.div`
+	grid-column: span 6;
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: span 4;
+	}
+`;
+
+const SmallTwoWrapper = styled.div`
+	grid-column: span 6;
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: span 4;
+	}
+`;
+
+const ImageComponentOneBigTwoSmall = (props: Props) => {
+	const { imageComponentOneBigTwoSmall } = props;
+	const { big, small1, small2 } = imageComponentOneBigTwoSmall;
+
 	return (
 		<ImageComponentOneBigTwoSmallWrapper>
-			ImageComponentOneBigTwoSmall
+			<LayoutWrapper useGalleryLayout>
+				<LayoutGrid useGalleryGrid>
+					<BigWrapper>
+						<MediaStack data={big} />
+					</BigWrapper>
+					<SmallOneWrapper>
+						<MediaStack data={small1} />
+					</SmallOneWrapper>
+					<SmallTwoWrapper>
+						<MediaStack data={small2} />
+					</SmallTwoWrapper>
+				</LayoutGrid>
+			</LayoutWrapper>
 		</ImageComponentOneBigTwoSmallWrapper>
 	);
 };
