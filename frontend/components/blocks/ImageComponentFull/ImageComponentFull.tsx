09e8140b822584d71398728e75e4b6b0d385347e
Tayte Boss <boss.tayte@gmail.com>
2024-03-22 10:34:58 +0000

    [style] Full - Structure and responsive styling

--- frontend/components/blocks/ImageComponentFull/ImageComponentFull.tsx
+++ frontend/components/blocks/ImageComponentFull/ImageComponentFull.tsx
@@ -1,11 +1,41 @@
 import styled from 'styled-components';
+import LayoutWrapper from '../../common/LayoutWrapper';
+import LayoutGrid from '../../common/LayoutGrid';
+import MediaStack from '../../common/MediaStack';
+
+type Props = {
+	imageComponentFull: any;
+};
 
 const ImageComponentFullWrapper = styled.section``;
 
-const ImageComponentFull = () => {
+const FullWrapper = styled.div`
+	grid-column: 1 / -1;
+
+	.image-component-wrapper,
+	.video-component-wrapper {
+		padding-top: 56.25%;
+	}
+`;
+
+const ImageComponentFull = (props: Props) => {
+	const { imageComponentFull } = props;
+	const { image, video, mediaType } = imageComponentFull;
+	const mediaData = {
+		image,
+		video,
+		mediaType
+	};
+
 	return (
 		<ImageComponentFullWrapper>
-			ImageComponentFull
+			<LayoutWrapper useGalleryLayout>
+				<LayoutGrid useGalleryGrid>
+					<FullWrapper>
+						<MediaStack data={mediaData} />
+					</FullWrapper>
+				</LayoutGrid>
+			</LayoutWrapper>
 		</ImageComponentFullWrapper>
 	);
 };
