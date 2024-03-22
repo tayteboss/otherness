006fed23417a74009edc86ccfedeafb74a3f2c76
Tayte Boss <boss.tayte@gmail.com>
2024-03-22 11:11:20 +0000

    [style] One Small One Big Landscape - Structure and responsive styling

--- frontend/components/blocks/ImageComponentOneSmallOneBigLandscape/ImageComponentOneSmallOneBigLandscape.tsx
+++ frontend/components/blocks/ImageComponentOneSmallOneBigLandscape/ImageComponentOneSmallOneBigLandscape.tsx
@@ -1,11 +1,81 @@
 import styled from 'styled-components';
+import LayoutWrapper from '../../common/LayoutWrapper';
+import LayoutGrid from '../../common/LayoutGrid';
+import MediaStack from '../../common/MediaStack';
+
+type StyledProps = {
+	$isLHS: boolean;
+	$gridColumn: string;
+};
+
+type Props = {
+	imageComponentOneSmallOneBigLandscape: any;
+};
 
 const ImageComponentOneSmallOneBigLandscapeWrapper = styled.section``;
 
-const ImageComponentOneSmallOneBigLandscape = () => {
+const SmallWrapper = styled.div<StyledProps>`
+	grid-column: ${(props) => props.$gridColumn};
+	order: ${(props) => (props.$isLHS ? 1 : 2)};
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: 1 / 5;
+	}
+
+	.image-component-wrapper,
+	.video-component-wrapper {
+		padding-top: 100%;
+	}
+`;
+
+const LandscapeWrapper = styled.div<StyledProps>`
+	grid-column: ${(props) => props.$gridColumn};
+	order: ${(props) => (props.$isLHS ? 1 : 2)};
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: 1 / -1;
+	}
+
+	.image-component-wrapper,
+	.video-component-wrapper {
+		padding-top: 62.3%;
+	}
+`;
+
+const ImageComponentOneSmallOneBigLandscape = (props: Props) => {
+	const { imageComponentOneSmallOneBigLandscape } = props;
+	const { landscape, small, selectPosition } =
+		imageComponentOneSmallOneBigLandscape;
+
+	let smallGridColumn = '';
+	let landscapeGridColumn = '';
+
+	if (selectPosition === 'left') {
+		smallGridColumn = '1 / 7';
+		landscapeGridColumn = '7 / -1';
+	} else if (selectPosition === 'right') {
+		smallGridColumn = '19 / -1';
+		landscapeGridColumn = '1 / 19';
+	}
+
 	return (
 		<ImageComponentOneSmallOneBigLandscapeWrapper>
-			ImageComponentOneSmallOneBigLandscape
+			<LayoutWrapper useGalleryLayout>
+				<LayoutGrid useGalleryGrid>
+					<SmallWrapper
+						$isLHS={selectPosition === 'left'}
+						$gridColumn={smallGridColumn}
+					>
+						<MediaStack data={small} />
+					</SmallWrapper>
+					<LandscapeWrapper
+						$isLHS={selectPosition === 'right'}
+						$gridColumn={landscapeGridColumn}
+					>
+						<MediaStack data={landscape} />
+					</LandscapeWrapper>
+				</LayoutGrid>
+			</LayoutWrapper>
 		</ImageComponentOneSmallOneBigLandscapeWrapper>
 	);
 };
